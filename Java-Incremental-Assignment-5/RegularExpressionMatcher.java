fc3744a482ef159212a5f4eb5e20b81bb4c4cf5c
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-03-26 17:54:58 +0530

    Create RegularExpressionMatcher.java

---
+++ Java-Incremental-Assignment-5/RegularExpressionMatcher.java
@@ -0,0 +1,24 @@
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class RegularExpressionMatcher{
+  public static void main(String[] args) {
+    String rule = "^[A-Z].*[.]$";
+    String input;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string for evaluation = ");
+    input = sc.nextLine();
+    Pattern p = Pattern.compile(rule);
+    Matcher m = p.matcher(input);
+    boolean b = m.matches();
+    if (b){
+      System.out.println("It matches with the pattern !");
+    }
+    else{
+      System.out.println("It doesn't match with the pattern !");
+    }
+
+  }
+}
