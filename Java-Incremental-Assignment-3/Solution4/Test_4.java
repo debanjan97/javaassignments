3756987f9b366a4b8c0aa71b4f85ac652163f92e
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-03-26 15:41:35 +0530

    Create Test_4.java

---
+++ Java-Incremental-Assignment-3/Solution4/Test_4.java
@@ -0,0 +1,41 @@
+interface Cycle{
+  public void balance();
+}
+class Unicycle implements Cycle{
+  public void balance(){
+    System.out.println("Hard");
+  }
+}
+class Bicycle implements Cycle{
+  public void balance(){
+    System.out.println("Medium");
+  }
+}
+class Tricycle implements Cycle{
+  public void balance(){
+    System.out.println("Easy");
+  }
+}
+class UnicycleFactory{
+  public Unicycle deployUnit(){
+    return new Unicycle();
+  }
+}
+class BicycleFactory{
+  public Bicycle deployUnit(){
+    return new Bicycle();
+  }
+}
+class TricycleFactory{
+  public Tricycle deployUnit(){
+    return new Tricycle();
+  }
+}
+
+public class Test_4{
+  public static void main(String[] args) {
+    UnicycleFactory factoryOne = new UnicycleFactory();
+    Unicycle cycle1 = factoryOne.deployUnit();
+    cycle1.balance();
+  }
+}
