859418e849247528263b8d81f9f44a132a07308f
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-03-27 15:55:08 +0530

    Create Test.java

---
+++ Incremental-Assignments-2/Solution3/Test.java
@@ -0,0 +1,12 @@
+class TestClass{
+	public TestClass(String s){
+		System.out.println("Message = "+s);
+	}
+}
+public class Test{
+	public static void main(String[] args) {
+		TestClass[] arrayObject = new TestClass[10];
+		//no, the messages aren't getting printed
+		}
+	}
+}
