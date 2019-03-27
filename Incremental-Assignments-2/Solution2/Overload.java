b66a36b1e01b21069e414aef8139eb8af0effcdb
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-03-27 15:54:09 +0530

    Create Overload.java

---
+++ Incremental-Assignments-2/Solution2/Overload.java
@@ -0,0 +1,16 @@
+class ClassOne{
+	public ClassOne(int arg1){
+		System.out.println("Argument 1 = "+arg1);
+		new ClassOne(4, 5);
+	}
+	public ClassOne(int arg1, int arg2){
+		int sum = arg1 + arg2;
+		System.out.println("Sum of both arguments = "+sum);
+	}
+}
+
+public class Overload{
+	public static void main(String[] args) {
+		ClassOne objectOne = new ClassOne(55);
+	}
+}
