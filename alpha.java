f0580a5768ea766e570feb2902534acfce4d9c7a
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2019-06-12 21:28:55 +0530

    Added comments

--- alpha.java
+++ alpha.java
@@ -1,30 +1,38 @@
-import java.util.*;
-import java.io.*;
-
 class Alpha{
-  public static void checkAll(String s){
-    boolean flag[] = new boolean[26]; //Automatically initialized to False
-    String lower_s = s.toLowerCase();
-    int count = 26;
-    for(int i=0; i<s.length(); i++){
-      int index = lower_s.charAt(i) - 97;
-      if(!flag[index]){
-        flag[index] = true;
-        count--;
+  public static void checkAll(String stringToBeTested){
+    // boolean array of size 26, storing if the character appeared or not in the string
+    boolean isPresent[] = new boolean[26]; 
+
+    // converting and storing the string in lower case
+    // it will not affect the case sensitivity
+    // it will make us deal with lower characters only
+    // no need of handling uppercase characters seperately
+    String lowerCaseString = stringToBeTested.toLowerCase();
+
+    int countOfAlphabetsNotAppeared = 26;
+    for(int i=0; i<stringToBeTested.length(); i++){
+	
+      // mapping the ascii values of alphabets to indexes of boolean array
+      int index = lowerCaseString.charAt(i) - 97;
+      if(!isPresent[index]){
+        isPresent[index] = true;
+        countOfAlphabetsNotAppeared--;
       }
     }
-    if(count == 0){
+
+    //no alphabet remains
+    if(countOfAlphabetsNotAppeared == 0){
       System.out.println("All characters are present !");
     }
     else{
       System.out.println("Not all characters are present !");
     }
-    //Time complexity O(n)
-    //Space Complexity O(n);
+    //Time  complexity O(n);
+    //Space complexity O(n);
   }
 
   public static void main(String[] args){
-    String s = "abCdEfghijKLMNopQrStuVWXY";
-    checkAll(s);
+    String stringToBeTested = "abCdEfghijKLMNopQrStuVWXYz";
+    checkAll(stringToBeTested);
   }
 }
